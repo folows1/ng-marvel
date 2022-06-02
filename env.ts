01f3aa4e33f69a93eebf383db0c6c3cd0d51dd18
folows1 <michelribeirocorrea@gmail.com>
2022-06-02 15:54:52 -0300

    test

--- env.ts
+++ env.ts
@@ -1,5 +1,4 @@
 export const environment = {
-    production: false,
-    API_URL: process.env.API_URL,
-    API_KEY: process.env.API_KEY,
+    API_URL: 'https://gateway.marvel.com/v1/public/',
+    API_KEY: '&ts=1000&apikey=918fa0509fabca6784c4c2376d6acb5e&hash=33582913d3d6652d99eea7be6145ce54',
 };
